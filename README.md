d4867e949451f35d2cc0dfa27786d59e320b72d8
aaronw-dev <riptidegamesceo@gmail.com>
2025-09-01 08:18:13 -0700

    basic db func

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# signup-hook
+# Overview
+This is the system that catches Typeform webhooks when a new member registers for DT Code Club. It works with a Firebase database and our `auto-email` system to send email to newly signed-up users as well.
+
+# Maintenance
+For future executive teams, I (aaronw-dev) recommend that you have at least one executive member that can do maintenance, bug fixes etc. on our code systems (e.g. website, auto-email, signup-hook).
+
+This system uses Vercel to deploy it. Please send me a DM on Discord or send me an email at [me@awdev.co](mailto:me@awdev.co) if you want access to it.
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+import random
+import string
+from datetime import datetime
+import os
+import json
 
 app = Flask(__name__)
+cert_json = os.environ.get("FIREBASE_CERT_JSON")
+if not cert_json:
+    raise RuntimeError("FIREBASE_CERT_JSON environment variable not set.")
+cert_dict = json.loads(cert_json)
+cred = credentials.Certificate(cert_dict)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
 @app.route("/")
@@ -8,13 +22,47 @@ def index():
     return "balls"
 
 
-@app.route("/api/webhook", methods=["POST"])
+def randomstring(n, alpha=True, numeric=True, symbolic=True):
+    chars = ""
+    if alpha:
+        chars += string.ascii_letters
+    if numeric:
+        chars += string.digits
+    if symbolic:
+        chars += string.punctuation
+    if not chars:
+        raise ValueError("At least one character set must be enabled.")
+    return ''.join(random.choices(chars, k=n))
+
+
+@app.route("/api/webhook")
 def webhook():
     data = request.json
-    print("Webhook received:", data)
-
-    # ✅ Acknowledge quickly so Typeform doesn’t retry
-    return jsonify({"status": "received"}), 200
+    main_form = data["form_response"]
+    formfields = main_form["definition"]["fields"]
+    fields = {}
+    answers = {}
+    for field in formfields:
+        title = field.get("title")
+        id = field.get("id")
+        fields[id] = title
+    for answer in main_form["answers"]:
+        answerid = answer["field"]["id"]
+        answer = answer[answer["type"]]
+        fieldname = fields[answerid]
+        answers[answerid] = {
+            "name": fieldname,
+            "value": answer
+        }
+    print(answers)
+    userinfo = {
+        "firstname": "Test Name",
+        "lastname": "Test Name",
+        "signup": datetime.utcnow()
+    }
+    userid = randomstring(20)
+    db.collection("members").add(userinfo, userid)
+    return {"status": "received"}
 
 
 if __name__ == "__main__":
